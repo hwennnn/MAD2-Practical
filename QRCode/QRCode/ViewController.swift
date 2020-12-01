b47e7758534854a5356c8f7daf42631e3cabdee1
hwen <whman63@gmail.com>
2020-12-01 12:47:11 +0800

    install pod dependencies and wrote some code testing one library

--- QRCode/QRCode/ViewController.swift
+++ QRCode/QRCode/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import QRCoder
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QRImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let generator = QRCodeGenerator()
+        //Default correction level is M
+        generator.correctionLevel = .H
+        QRImage.image = generator.createImage(value: "Hello world!",size: CGSize(width: 200,height: 200))!
+        
     }
 
 
